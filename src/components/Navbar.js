c1749eb468c6515606ce2cf78ecd4f88da63e15b
RainStorm108 <57578799+RainStorm108@users.noreply.github.com>
2023-05-15 21:56:03 -0700

    fix navbar highlight and search font color

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,36 +1,10 @@
 import React, {useState} from "react";
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 import "./Navbar.css"
 
 // ant designs
 import {MailOutlined, SettingOutlined , AppstoreOutlined} from '@ant-design/icons';
-import { Menu, Switch , Input, Button} from 'antd';
-
-function getItem(label, key, icon, children, type) {
-  return {
-    key,
-    icon,
-    children,
-    label,
-    type,
-  };
-}
-
-// TODO: remove this and replace with ant design
-// const items = [
-//   getItem('Home', 'sub1', <MailOutlined />, [
-//     getItem('Option 1', '1'),
-//     getItem('Option 2', '2'),
-//   ]),
-//   getItem('Projects', 'sub2', <AppstoreOutlined />, [
-//     getItem('Option 5', '5'),
-//     getItem('Submenu', 'sub3', null, [getItem('Option 7', '7'), getItem('Option 8', '8')]),
-//   ]),
-//   getItem('About', 'sub4', <SettingOutlined />, [
-//     getItem('Option 9', '9'),
-//     getItem('Switch Dark Mode', '10', null, null, 'switch'),
-//   ]),
-// ];
+import { Menu, Switch, Input} from 'antd';
 
 function NavBar() {
   const [theme, setTheme] = useState('dark');
@@ -43,32 +17,49 @@ function NavBar() {
     setCurrent(e.key);
   };
 
+  const location = useLocation();
+
+  const getCurrentPageKey = () => {
+    const path = location.pathname;
+    switch (path) {
+      case '/':
+        return 'home';
+      case '/about':
+        return 'about';
+      case '/contact':
+        return 'contact';
+      case '/projects':
+        return 'projects';
+      default:
+        return '';
+    }
+  };
+  const selectedKey = getCurrentPageKey();
+  
   // TODO: Pass the theme state to routes and change the theme of the page, remove the old index.css file
   return (
-    <Menu mode="horizontal" selectedKeys={[current]} theme={theme} style={{ display: 'flex', justifyContent: 'flex-end' }}>
-      <Menu.Item key="1">
+    <Menu mode="horizontal" selectedKeys={[selectedKey]} theme={theme} style={{ display: 'flex', justifyContent: 'flex-end' }}>
+      <Menu.Item key="home">
         <Link to="/">Home</Link>
       </Menu.Item>
-      <Menu.Item key="2">
+      <Menu.Item key="about">
         <Link to="/about">About</Link>
       </Menu.Item>
-      <Menu.Item key="3">
+      <Menu.Item key="projects">
         <Link to="/projects">Projects</Link>
       </Menu.Item>
-      <Menu.Item key="4">
+      <Menu.Item key="contact">
         <Link to="/contact">Contact</Link>
       </Menu.Item>
 
       <Menu.Item key="5">
-        <Input placeholder="Search..." onSearch={(value) => console.log(value)} style={{width: 300, 
-        backgroundColor: theme === 'dark' ? '#fff' : '#fff',
-        color: theme === 'dark' ? '#fff' : '#fff',
-        border: theme === 'dark' ? 'none' : '1px solid #ccc'}} />
-
-        <Button type="primary" style={{
-        backgroundColor: theme === 'dark' ? '#4096ff' : '#69b1ff',
-        color: theme === 'dark' ? '#fff' : '#fff',
-        border: theme === 'dark' ? 'none' : '1px solid #ccc'}}>Search</Button>
+        <Input.Search
+        placeholder="input search text"
+        allowClear
+        enterButton="Search"
+        onSearch={(value) => console.log(value)}
+        style={{ width: 300 , marginTop: 8}} // TODO: fix this, the search bar is not aligned horizontally with other navbar items
+        />
       </Menu.Item>
 
       <Menu.Item key="6" style={{ float: 'right' }}>
