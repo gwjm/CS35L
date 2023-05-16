38e85624ebed051b1efd49bd671c9fb90f484c78
RainStorm108 <57578799+RainStorm108@users.noreply.github.com>
2023-05-16 15:54:12 -0700

    added boxshadow to navbar, removed old navbar code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -40,7 +40,7 @@ function NavBar() {
 
   // TODO: Pass the theme state to routes and change the theme of the page, remove the old index.css file
   return (
-    <Menu mode="horizontal" selectedKeys={[selectedKey]} theme={theme} style={{ display: 'flex' , justifyContent: 'space-between'}} onClick={onClick}>
+    <Menu mode="horizontal" selectedKeys={[selectedKey]} theme={theme} style={{ display: 'flex' , justifyContent: 'space-between' , boxShadow: "5px 8px 24px 5px rgba(208, 216, 243, 0.6)" }} onClick={onClick}>
       <div>
       <Menu.Item key="logo" style={{ fontWeight: 'bold' }}>
       <Link to="/">{<PandaIcon
@@ -94,56 +94,6 @@ function NavBar() {
   );
 }
 
-// TODO: remove this and replace with ant design, this is the old navbar
-// function NavBar() {
-//   const { Search } = Input;
-//   const [click , setClick] = useState(false);
-
-//   const handleClick = () => setClick(!click);
-//   const closeMobileMenu = () => setClick(false)
-//   return (
-//     <>
-//     <IconContext.Provider value={{color : "#fff"}}>
-//     <nav className="navbar">
-//       <div className="navbar-container container">
-//         <Link to="/" className="navbar-logo" >
-//           <BiBookOpen className="navbar-icon" onClick={closeMobileMenu} />
-//           ProjectHub
-//         </Link>
-//         <div className="menu-icon" onClick={handleClick}>
-//           {click ? <FaTimes /> : <FaBars />}
-//         </div>
-        
-//         <ul className={click ? "nav-menu active" : "nav-menu"}>
-//           {createNavElement("/", (<HomeOutlined /> ), closeMobileMenu)}
-//           {createNavElement("/projects", "Projects", closeMobileMenu)}
-//           {createNavElement("/contact", "Contact", closeMobileMenu)}
-//           {createNavElement("/about", "About", closeMobileMenu)}
-//           <li className="search-bar">
-//             <Search placeholder="input search text" onSearch={(value) => console.log(value)} style={{ width:300 }} enterButton />
-//           </li>
-//         </ul>
-//       </div>
-//     </nav>
-//     </IconContext.Provider>
-//     </>
-//   )
-// }
-
-// const createNavElement = (pathTo, label, clickAction) => {
-//   return (<li className="nav-item">
-//     <NavLink 
-//       to={pathTo}
-//       className={({ isActive }) => 
-//         "nav-links" + (isActive ? " activated" : "")
-//         }
-//       onClick={clickAction}
-//       >
-//         {label}
-//     </NavLink>
-//   </li>
-//   );
-// }
 
 const PandaSvg = () => (
   <svg viewBox="0 0 1024 1024" width="1em" height="1em" fill="currentColor">
