bf4bd29adcd094e22d1c437698824474e8fd30be
gwjm <gregor@the-macdonalds.net>
2023-05-19 18:49:23 -0700

    Created Task Schema

---
+++ backend/models/task.model.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const taskSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    assignedUsers: {
+        type: mongoose.schema.Types.ObjectId,
+        required: true
+    }
+})
+
+const Task = mongoose.model('Tasks', taskSchema)
+
+module.exports = Task;
