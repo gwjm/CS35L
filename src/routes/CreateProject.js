5f339656e7db524f30d890fd538476969f9c1d2a
Solomon Kwon <solomonckwon@gmail.com>
2023-05-27 19:37:32 -0700

    forogt to add renamed file

--- src/routes/CreateProject.js
+++ src/routes/CreateProject.js
@@ -1,156 +1,112 @@
-//import React from "react";
-import { ClockCircleOutlined } from '@ant-design/icons';
-import { Timeline , ConfigProvider} from 'antd';
-import { DatePicker, Space , Layout} from 'antd';
+import React, { useState, useEffect, useContext } from 'react';
+import { Form, Input, Button, DatePicker, Select } from 'antd';
 import axios from 'axios';
+import { AlignCenterOutlined } from '@ant-design/icons';
+import AuthContext from "../contexts/AuthProvider.js";
 
-import React, { Component } from 'react';
-//import axios from 'axios';
-//import DatePicker from 'react-datepicker';
-//import "react-datepicker/dist/react-datepicker.css";
+const { Option } = Select;
 
-export default class CreateProject extends Component {
-  constructor(props) {
-    super(props);
 
-    // this.onChangeDate = this.onChangeDate.bind(this);
-    this.onChangeDeadline = this.onChangeDeadline.bind(this);
-    this.onChangeDescription = this.onChangeDescription.bind(this);
-    this.onChangeOwner = this.onChangeOwner.bind(this);
-    this.onChangeTitle = this.onChangeTitle.bind(this);
-    //this.onChangeMembers = this.onChangeMembers.bind(this);
-    //this.onChangePermissions = this.onChangePermissions.bind(this);
-    this.onSubmit = this.onSubmit.bind(this)
 
-    this.state = {
-      title: '',
-      owner: '',
-      description: '',
-      duration: 0,
-      deadline: new Date(),
-      users: [],
-      usersIDs: [],
+const ProjectForm = () => {
+  const { auth } = useContext(AuthContext);
+  const [members, setMembers] = useState([]);
+
+  
+
+  const onFinish = async values => {
+    console.log('Success:', values);
+    try {
+      const userlogged = await axios.get(`http://localhost:3001/api/users/findusername/${auth.user1}`);
+      const data = { ...values, owner: userlogged.data._id }; // Add the owner field with the logged-in user's _id
+      console.log(data)
+      await axios.post('http://localhost:3001/api/projects/add', data);
+      console.log('Project created successfully');
+    } catch (error) {
+      console.error(error);
     }
-  }
+  };
 
-componentDidMount() {
-  axios.get('http://localhost:3001/api/users/')
-        .then(response => {
-          console.log(response.data)
-          if (response.data.length > 0) {
-            this.setState({
-              users: response.data.map(user => user.username),
-              usersIDs: response.data.map(user => user._id),
-            })
-          }
-        })
-        .catch((error) => {
-          console.log(error);
-        })
-  }
+  const onFinishFailed = errorInfo => {
+    console.log('Failed:', errorInfo);
+  };
+  const fetchMembers = async () => {
+    try {
+      const response = await axios.get(`http://localhost:3001/api/users/`);
+      setMembers(response.data);
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-onChangeDate(date) {
-  this.setState({ date: date});
-}
+  useEffect(() => {
+    fetchMembers(); // Call fetchMembers when the component is mounted
+  }, []); // The empty dependency array [] ensures that the effect runs only once on component mount
 
-onChangeDeadline(date) {
-  this.setState({ deadline: date});
-}
 
-onChangeDescription(e) {
-  this.setState({ description: e.target.value});
-}
+  return (
+    <Form
+      name="basic"
+      labelCol={AlignCenterOutlined}
+      wrapperCol={{ span: 16 }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+    >
+      <Form.Item
+        label="Project Name"
+        name="title"
+        rules={[{ required: true, message: 'Please input your project name!' }]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        label="Description"
+        name="description"
+        rules={[{ required: true, message: 'Please input your project description!' }]}
+      >
+        <Input.TextArea />
+      </Form.Item>
+      <Form.Item
+        label="Members"
+        name="members"
+        rules={[{ required: true, message: 'Please select project members!' }]}
+      >
+        <Select
+          mode="multiple"
+          showSearch
+          // onSearch={fetchMembers}
+          filterOption={false}
+          placeholder="Select members"
+        >
+          {members.map(member => (
+            <Option key={member._id} value={member._id}>
+              {member.username}
+            </Option>
+          ))}
+        </Select>
+      </Form.Item>
+      <Form.Item
+        label="Deadline"
+        name="deadline"
+        rules={[{ required: true, message: 'Please select a deadline!' }]}
+      >
+        <DatePicker />
+      </Form.Item>
+      <Form.Item
+        label="Task List"
+        name="taskList"
+        rules={[{ required: false, message: 'Please input a task list!' }]}
+      >
+        <Input.TextArea />
+      </Form.Item>
+      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
 
-//not sure about members and permissions
-
-onChangeOwner(e) {
-  this.setState({owner: e.target.value});
-}
-
-onChangeTitle(e) {
-  this.setState({title: e.target.value});
-}
-
-onSubmit(e) {
-  e.preventDefault();
-
-  const project = {
-    // date: new Date(),
-    deadline: new Date(),
-    title: this.state.title,
-    description: this.state.description,
-    members: [this.state.owner],
-    owner: this.state.owner,
-  }
-
-  for (var i = 0; i < this.state.users.length; i++) {
-    if (project.owner == this.state.users[i]) {
-      console.log("HEREEEEEEEEEEEEEEE")
-      project.owner = this.state.usersIDs[i];
-      project.members = this.state.usersIDs[i];
-  }}
-
-  axios.post('http://localhost:3001/api/projects/add', project).then(rest => console.log('Added Project'));
-
-  //change following line to section where project added to database
-  console.log(project)
-}
-
-  render() {
-    return (
-      <div>
-      <h3>Create New Project</h3>
-      <form onSubmit={this.onSubmit}>
-        <div className="form-group"> 
-          <label>Username: </label>
-          <select ref="userInput"
-              required
-              className="form-control"
-              value={this.state.owner}
-              onChange={this.onChangeOwner}>
-              <option value="">Select User</option>
-              {
-                this.state.users.map(function(user) {
-                  return <option 
-                    key={user}
-                    value={user}>{user}
-                    </option>;
-                })
-              }
-          </select>
-        </div>
-        <div className="form-group"> 
-          <label>Title: </label>
-          <input  type="text" placeholder="What's the task title?"
-              required
-              className="form-control"
-              value={this.state.title}
-              onChange={this.onChangeTitle}
-              />
-        </div>
-        <div className="form-group"> 
-          <label>Description: </label>
-          <input  type="text" placeholder="Describe the task"
-              required
-              className="form-control"
-              value={this.state.description}
-              onChange={this.onChangeDescription}
-              />
-        </div>
-        <div className="form-group">
-          <label>Deadline: </label>
-          <div>
-            <DatePicker
-              selected={this.state.deadline}
-              onChange={this.onChangeDeadline}
-            />
-          </div>
-        </div>
-
-        <div className="form-group">
-          <input type="submit" value="Create Project" className="btn btn-primary" />
-        </div>
-      </form>
-    </div>);
-  }
-}
+export default ProjectForm;
