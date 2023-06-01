c151f25e0658392bc9b7c4da0f2b1afdc751feb8
RainStorm108 <jerryhuang1230@gmail.com>
2023-06-01 01:15:55 -0700

    Grace home page quote

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,44 @@
-import React from "react";
+
 import { Card } from 'antd';
 import backgroundImage from "../styles/image1.jpg";
 import { useTheme, useThemeUpdate } from "../contexts/ThemeContext";
 import { ConfigProvider , theme } from 'antd';
+import React, { useState, useEffect } from 'react';
+import { Typography } from 'antd';
+
+const { Text } = Typography;
+
+const TypingAnimation = () => {
+  const [text, setText] = useState('');
+  const fullText = "TThis is Grace's favorite quote and color. Don't tell her I told you.";
+
+  useEffect(() => {
+    let isMounted = true;
+    let currentIndex = 0;
+  
+    const typeText = () => {
+        if (currentIndex < fullText.length - 1) {
+          if (isMounted) {
+            setText((prevText) => prevText + fullText[currentIndex]);
+            currentIndex++;
+            setTimeout(typeText, 100); // Adjust typing speed here (in milliseconds)
+          } else {
+            currentIndex = fullText.length; // Stop the animation if component is unmounted
+          }
+        }
+      };
+      
+  
+    typeText();
+  
+    return () => {
+      isMounted = false;
+    };
+  }, []);
+  
+  return <Text strong>{text}</Text>;
+};
+
 
 const { defaultAlgorithm, darkAlgorithm } = theme;
 
@@ -29,6 +65,7 @@ const Home = (props) => {
                             <span>Hub</span>
                         </h1>
                         <p>I just picked a shade</p>
+                        <TypingAnimation />
                     </center>
                 </div>
             </Card>
