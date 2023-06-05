199442ad30abe9d8443eb8d20f581c0145a2c3bd
RainStorm108 <jerryhuang1230@gmail.com>
2023-06-05 14:20:49 -0700

    Update randomColors.js
    More colors

--- src/utility/randomColors.js
+++ src/utility/randomColors.js
@@ -1,19 +1,21 @@
-
-// Helper function to get a random color from Ant Design's color palette
 export const getRandomColor = () => {
     // Ant design colors for user tags
     const antDesignColors = [
-        'pink',
-        'red',
-        'yellow',
-        'orange',
-        'cyan',
-        'green',
-        'blue',
-        'purple',
-        'lime',
-        'gold',
+      'pink',
+      'red',
+      'yellow',
+      'orange',
+      'cyan',
+      'green',
+      'blue',
+      'purple',
+      'lime',
+      'gold',
+      'volcano',
+      'geekblue',
+      'magenta',
     ];
     const randomIndex = Math.floor(Math.random() * antDesignColors.length);
     return antDesignColors[randomIndex];
-};
+  };
+  
