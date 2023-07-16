1157fb57e7cbb98f5ee1339d028c9d3596227329
RainStorm108 <57578799+RainStorm108@users.noreply.github.com>
2023-07-16 15:20:31 -0700

    Update startWeb.sh
    Kill process by pid after control c

--- startWeb.sh
+++ startWeb.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
+
+# Function to handle Ctrl+C
+function cleanup() {
+    echo "Terminating processes..."
+    kill $frontend_pid $backend_pid
+    exit 0
+}
+
+# Install frontend dependencies
 npm install
 
 # Start the frontend
 npm start &
+frontend_pid=$!
 
-# Start the backend
-# shellcheck disable=SC2164
+# Install backend dependencies
 cd ./backend/
 npm install
-nodemon start
+
+# Start the backend
+nodemon start &
+backend_pid=$!
+
+# Trap Ctrl+C and call the cleanup function
+trap cleanup INT
+
+# Wait for the processes to finish
+wait
